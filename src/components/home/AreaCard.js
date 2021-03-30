5fa9d89a06d1f0d2db5571498b7bd91102ff9f3c
lulaluano <luanferreira2136@gmail.com>
2021-03-30 17:48:25 -0300

    Ajustes na parte de Áreas de Atuação

--- src/components/home/AreaCard.js
+++ src/components/home/AreaCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function AreaCard(props) {
     return (
-        <div className="col-4">
-            <a className="area-card bg-white" href="#">
+        <div className="col-4 shadow-col">
+            <div className="area-card cursor-pointer bg-white">
                 <img className="d-block pb-3" src={`/img/area-cards/${props.icon}.svg`} alt={props.icon} />
                 <div className="text-escuro playfair fs-22">{props.title}</div>
-                <div className="py-3 text-normal">{props.children}</div>
-                <span className="text-dourado">Ler Mais</span>
-            </a>
+                <div className="py-4 text-normal">{props.children}</div>
+                <a href="#" className="ler-mais">Ler Mais <span className="flecha">&rarr;</span></a>
+            </div>
         </div>
     )
 }
--- src/components/home/Areas.js
+++ src/components/home/Areas.js
@@ -7,24 +7,25 @@ export default function Areas({ areas }) {
     return (
         <div className="container-fluid px-0 fundo-areas">
             <div className="container">
-                <div className="row g-3">
-                    <div className="col-4">
-                        <span className="line-title w-max-content">NOSSOS SERVIÇOS</span>
-                        <strong className="playfair fs-44 d-block text-escuro mb-3">Áreas de Atuação</strong>
-                        <span>
-                            Realizando a prestação de serviços jurídicos tanto para empresas como pessoas físicas, o BRC
-                            Advogados elabora soluções jurídicas de forma singularizada, estruturando operações empresariais
-                        </span>
-                    </div>
-                    <div className="col-4">
-                        <div className="d-flex align-items-end h-100">
-                            <span className="d-inline-flex text-2">
-                                <span>
-                                    de alta complexidade, realizando assessoria jurídica cível, trabalhista e em LGPD,
-                                além da atuação em processos perante em todas as instâncias e tribunais espalhados
-                                pelo país.
+                <div className="row g-4">
+                    <div className="col-8">
+                        <div className="d-flex flex-column">
+                            <span className="line-title w-max-content">NOSSOS SERVIÇOS</span>
+                            <strong className="playfair fs-44 d-block text-escuro my-15-50">Áreas de Atuação</strong>
+
+                            <div className="d-flex">
+                                <span className="d-inline-flex pres-text mr-40">
+                                    Realizando a prestação de serviços jurídicos tanto para empresas como pessoas
+                                    físicas, o BRC Advogados elabora soluções de forma singularizada, estruturando
+                                    operações empresariais de alta
                                 </span>
-                            </span>
+
+                                <span className="d-inline-flex pres-text">
+                                    complexidade, realizando assessoria jurídica cível, trabalhista e em LGPD, além da
+                                    atuação em processos perante em todas as instâncias e tribunais espalhados pelo
+                                    país.
+                                </span>
+                            </div>
                         </div>
                     </div>
 
@@ -32,7 +33,7 @@ export default function Areas({ areas }) {
                         areas.map(area => {
                             return (
                                 <AreaCard key={area.id} title={area.titulo} icon={area.icone}>
-                                    { area.descricao.substring(0, 95) + '...' }
+                                    { area.descricao.substring(0, 95) + '...    ' }
                                 </AreaCard>
                             )
                         })
