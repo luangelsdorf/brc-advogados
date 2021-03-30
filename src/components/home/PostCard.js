bf7198dff4b18bbda8040b29fd328e7deb75a4d6
lulaluano <luanferreira2136@gmail.com>
2021-03-30 19:46:50 -0300

    Ajustes nos cards de Blog

--- src/components/home/PostCard.js
+++ src/components/home/PostCard.js
@@ -6,14 +6,14 @@ export default function PostCard(props) {
             <div>
                 <div className="post-card-overlay">
                     <img src={`/img/card-img/${props.img}.png`} alt="Card Img"/>
-                    <span className="categoria">{props.categoria}</span>
+                    <a href="#" className="categoria px-3">{props.categoria}</a>
                 </div>
                 <div className="post-card-body flex-center">
                     <div>
                         <span className="d-block text-dourado mt-4">{props.data}</span>
-                        <span className="d-block playfair text-escuro fs-22 mt-4 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        <a href="#" className="d-block playfair post-title-link fs-22 mt-4 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a>
                         <span className="d-block mb-3">Lorem ipsum dolor sit amet, consectetur et adipiscing elit, sed do eiusmod quis tempor inunt ut labore et dolore magna aliqua...</span>
-                        <a href="#" className="text-dourado">Ler Mais</a>
+                        <a href="#" className="ler-mais">Ler Mais <span className="flecha">&rarr;</span></a>
                     </div>
                 </div>
             </div>
