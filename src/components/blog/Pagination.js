b88637b2ae2fdca10646837efca5b423db87c3c0
lulaluano <luanferreira2136@gmail.com>
2021-03-30 21:44:31 -0300

    Ajustes na página de Blog

--- src/components/blog/Pagination.js
+++ src/components/blog/Pagination.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Pagination() {
     return (
         <div className="d-flex justify-content-center" id="pagination">
-            <a href="#" className="pag-btn"><span className="fas fa-arrow-left" /></a>
+            <a href="#" className="pag-btn fs-13"><span className="fas fa-arrow-left" /></a>
 
-            <a href="#" className="pag-btn">1</a>
+            <a href="#" className="pag-btn active">1</a>
             <a href="#" className="pag-btn">2</a>
             <a href="#" className="pag-btn">3</a>
 
-            <a href="#" className="pag-btn"><span className="fas fa-arrow-right" /></a>
+            <a href="#" className="pag-btn fs-13"><span className="fas fa-arrow-right" /></a>
         </div>
     )
 }
