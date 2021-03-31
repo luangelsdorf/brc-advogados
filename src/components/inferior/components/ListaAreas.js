8980c52a0e31ec5d9df098dd43a1bf64f42599c2
lulaluano <luanferreira2136@gmail.com>
2021-03-31 16:27:00 -0300

    Ajustes nos espaçamentos da lista de áreas

--- src/components/inferior/components/ListaAreas.js
+++ src/components/inferior/components/ListaAreas.js
@@ -6,18 +6,15 @@ export default function ListaAreas() {
             <span className="list-title d-block playfair fs-20">Áreas de Atuação</span>
             <div className="d-flex">
                 <ul className="first-col">
-                    <li><a href="#">Direito de Família</a></li>
-                    <li><a href="#">Contencioso Cível</a></li>
-                    <li><a href="#">Direito Trabalhista</a></li>
-                    <li><a href="#">Previdenciário</a></li>
-                    <li><a href="#">Direito e Saúde</a></li>
+                    <li className="area-item"><a href="#">Consultivo Cível</a></li>
+                    <li className="area-item"><a href="#">Direito Empresarial</a></li>
+                    <li className="area-item"><a href="#">LGPD</a></li>
+                    <li className="area-item"><a href="#">Direito Administrativo</a></li>
                 </ul>
                 <ul>
-                    <li><a href="#">Propriedade Intelectual</a></li>
-                    <li><a href="#">Direito Administrativo</a></li>
-                    <li><a href="#">Consultivo Cível e Contratos</a></li>
-                    <li><a href="#">Societário</a></li>
-                    <li><a href="#">LGPD</a></li>
+                    <li className="area-item"><a href="#">Contencioso Civil</a></li>
+                    <li className="area-item"><a href="#">Trabalhista e Previdenciário</a></li>
+                    <li className="area-item"><a href="#">Direito e Saúde</a></li>
                 </ul>
             </div>
         </div>
