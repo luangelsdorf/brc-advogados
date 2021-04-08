43612eb86ef675eae6c4b5551a7b88a2cac2df1b
lulaluano <luanferreira2136@gmail.com>
2021-04-07 21:57:48 -0300

    Corrigidos links no rodapé

--- src/components/inferior/components/ListaAreas.js
+++ src/components/inferior/components/ListaAreas.js
@@ -6,15 +6,15 @@ export default function ListaAreas({ textos, areas }) {
             <span className="list-title d-block playfair fs-20">Áreas de Atuação</span>
             <div className="d-flex">
                 <ul className="first-col">
-                    <li className="area-item"><a href="/areas-de-atuacao/consultivo-civil">{ areas[0].titulo }</a></li>
-                    <li className="area-item"><a href="/areas-de-atuacao/empresarial-propriedade-intelectual">{ areas[1].titulo }</a></li>
-                    <li className="area-item"><a href="/areas-de-atuacao/lgpd">{ areas[2].titulo }</a></li>
-                    <li className="area-item"><a href="/areas-de-atuacao/direito-administrativo">{ areas[3].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/consultivo-civil`}>{ areas[0].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/empresarial-propriedade-intelectual`}>{ areas[1].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/lgpd`}>{ areas[2].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/direito-administrativo`}>{ areas[3].titulo }</a></li>
                 </ul>
                 <ul>
-                    <li className="area-item"><a href="/areas-de-atuacao/contencioso-civil">{ areas[4].titulo }</a></li>
-                    <li className="area-item"><a href="/areas-de-atuacao/trabalhista-previdenciario">{ areas[5].titulo }</a></li>
-                    <li className="area-item"><a href="/areas-de-atuacao/direito-e-saude">{ areas[6].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/contencioso-civil`}>{ areas[4].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/trabalhista-previdenciario`}>{ areas[5].titulo }</a></li>
+                    <li className="area-item"><a href={`/${textos.lang}/areas-de-atuacao/direito-e-saude`}>{ areas[6].titulo }</a></li>
                 </ul>
             </div>
         </div>
