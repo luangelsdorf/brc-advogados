1a5f0b1c940a2476d6f4fcf1b21afd1c3c49cf22
lulaluano <luanferreira2136@gmail.com>
2021-04-12 23:03:48 -0300

    hotfix

--- src/components/home/Parallax.js
+++ src/components/home/Parallax.js
@@ -4,7 +4,7 @@ export default function Parallax({ textos }) {
     return (
         <div className="container-fluid p-0 parallax-container text-white flex-center flex-column">
             <strong className="playfair fs-32 d-block">{ textos.parallax_text_1 }</strong>
-            <span className="d-block text-center my-20-30" id="parallax-text">
+            <span className="d-block w-100 text-center my-20-30" id="parallax-text">
                 { textos.parallax_text_2 }
             </span>
             <a className="btn btn-secondary btn-h-50" href="#banner-inferior">{ textos.btn_fale }</a>
