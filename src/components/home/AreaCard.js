54cf2b0eb5edc9c3ae55437673396fa882165b33
lulaluano <luanferreira2136@gmail.com>
2021-04-27 22:09:52 -0300

    Card de Área inteiramente clicável

--- src/components/home/AreaCard.js
+++ src/components/home/AreaCard.js
@@ -4,14 +4,14 @@ import Link from 'next/link';
 export default function AreaCard(props) {
     return (
         <div className="col-10 col-md-6 col-lg-4">
-            <div className="area-card cursor-pointer bg-white">
-                <img className="d-block pb-3" src={`/img/area-cards/${props.icon}.svg`} alt={props.icon} />
-                <div className="text-escuro playfair fs-22">{props.title}</div>
-                <div className="py-4 text-normal">{props.children}</div>
-                <Link href={props.href}>
-                    <a className="ler-mais">{props.lerMais} <span className="flecha">&rarr;</span></a>
-                </Link>
-            </div>
+            <Link href={props.href}>
+                <a className="area-card cursor-pointer bg-white">
+                    <img className="d-block pb-3" src={`/img/area-cards/${props.icon}.svg`} alt={props.icon} />
+                    <div className="text-escuro playfair fs-22">{props.title}</div>
+                    <div className="py-4 text-normal">{props.children}</div>
+                    <span className="ler-mais">{props.lerMais} <span className="flecha">&rarr;</span></span>
+                </a>
+            </Link>
         </div>
     )
 }
