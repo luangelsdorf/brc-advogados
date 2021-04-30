a62e5faebcdcf2f8edafab072a06b26c45a7c32b
lulaluano <luanferreira2136@gmail.com>
2021-04-30 20:43:35 -0300

    Aceitando somente requisições 'POST' na API

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -37,38 +37,42 @@ export default function (req, res) {
 require('dotenv').config()
 
 export default function (req, res) {
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: 'edpereira6161@gmail.com',
-            pass: 'ed-pereira-61',
-        },
-        secure: true,
-    });
+    if (req.method === 'POST') {
+        let nodemailer = require('nodemailer')
+        const transporter = nodemailer.createTransport({
+            port: 465,
+            host: "smtp.gmail.com",
+            auth: {
+                user: 'edpereira6161@gmail.com',
+                pass: 'ed-pereira-61',
+            },
+            secure: true,
+        });
 
-    const mailData = {
-        from: 'edpereira6161@gmail.com',
-        to: 'luanferreira2136.@gmail.com',
-        subject: `Nova Mensagem | Formulário Site BRC`,
-        text: `Mensagem: ${req.body.message} | Enviado de ${req.body.email}`,
-        html: `
+        const mailData = {
+            from: 'edpereira6161@gmail.com',
+            to: 'luanferreira2136.@gmail.com',
+            subject: `Nova Mensagem | Formulário Site BRC`,
+            text: `Mensagem: ${req.body.message} | Enviado de ${req.body.email}`,
+            html: `
                 <div>Você recebeu uma nova mensagem no seu site. Confira as informações abaixo:</div>
                 <p><strong>Nome</strong>: ${req.body.name}</p>
                 <p><strong>E-mail</strong>: ${req.body.email}</p>
                 <p><strong>Área de Atuação</strong>: ${req.body.area}</p>
                 <p><strong>Mensagem</strong>: ${req.body.message}</p>
               `
+        }
+
+        transporter.sendMail(mailData, function (err, info) {
+            if(err)
+                console.log(err)
+            else
+                console.log(info);
+        })
+
+
+        res.send('success')
+    } else {
+        res.status(405).send('405 - Method Not Allowed.')
     }
-
-    transporter.sendMail(mailData, function (err, info) {
-        if(err)
-            console.log(err)
-        else
-            console.log(info);
-    })
-
-
-    res.send('success')
 }
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -37,35 +37,39 @@ export default function (req, res) {
 require('dotenv').config()
 
 export default function (req, res) {
-    let nodemailer = require('nodemailer')
-    const transporter = nodemailer.createTransport({
-        port: 465,
-        host: "smtp.gmail.com",
-        auth: {
-            user: 'edpereira6161@gmail.com',
-            pass: 'ed-pereira-61',
-        },
-        secure: true,
-    });
+    if (req.method === 'POST') {
+        let nodemailer = require('nodemailer')
+        const transporter = nodemailer.createTransport({
+            port: 465,
+            host: "smtp.gmail.com",
+            auth: {
+                user: 'edpereira6161@gmail.com',
+                pass: 'ed-pereira-61',
+            },
+            secure: true,
+        });
 
-    const mailData = {
-        from: 'edpereira6161@gmail.com',
-        to: 'luanferreira2136.@gmail.com',
-        subject: `Nova Inscrição | Newsletter Site BRC`,
-        text: `Nova inscrição para ${req.body.email}`,
-        html: `
+        const mailData = {
+            from: 'edpereira6161@gmail.com',
+            to: 'luanferreira2136.@gmail.com',
+            subject: `Nova Inscrição | Newsletter Site BRC`,
+            text: `Nova inscrição para ${req.body.email}`,
+            html: `
                 <div>Nova inscrição recebida:</div>
                 <p><strong>E-mail</strong>: ${req.body.email}</p>
               `
+        }
+
+        transporter.sendMail(mailData, function (err, info) {
+            if(err)
+                console.log(err)
+            else
+                console.log(info);
+        })
+
+
+        res.send('success')
+    } else {
+        res.status(405).send('405 - Method not Allowed.')
     }
-
-    transporter.sendMail(mailData, function (err, info) {
-        if(err)
-            console.log(err)
-        else
-            console.log(info);
-    })
-
-
-    res.send('success')
 }
