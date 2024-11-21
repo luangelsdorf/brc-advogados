38fbb17448a7c519afed6dd6902aac5a377c3512
luangelsdorf <luanferreira2136@gmail.com>
2024-11-21 17:13:14 -0300

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   env: {
     api_url: process.env.api_url,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/pt',
+        permanent: false,
+      },
+    ]
+  },
 }
